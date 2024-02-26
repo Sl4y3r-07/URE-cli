f2556912c7ef214195c82430780f6715b46fa1cb
it4ch1 <akshitdavps@gmail.com>
2024-02-26 19:10:35 +0530

    names done

--- header.cpp
+++ header.cpp
@@ -52,6 +52,29 @@ std::string info_finder(int num,FILE* file)
 {
     return(little_to_big_endian(finder(num,file)));
 }
+void printNames(int NameOffset,int NameCount,FILE* file){
+    fseek(file,NameOffset + 4,SEEK_SET);
+    for(int i=0;i<NameCount;i++){
+        std::string name;
+        std::string NonCasePreservingHash;
+        std::string CasePreservingHash;
+        
+        char ch;
+        while((ch = getc(file))!=EOF && ch!=0){
+            if(ch!='\x00'){
+                name += ch;
+            }
+        }
+        NonCasePreservingHash = info_finder(2,file);
+        CasePreservingHash = info_finder(2,file);
+        std::cout<<"\tName "<<i<<"\t"<<name<<std::endl;
+        std::cout<<"\t\tCasePreservingHash:\t"<<stoul(CasePreservingHash,0,16)<<std::endl;
+        std::cout<<"\t\tNonCasePreservingHash:\t"<<stoul(NonCasePreservingHash,0,16)<<std::endl;
+        fseek(file,ftell(file)+4,SEEK_SET);
+    }
+
+}
+
 int main(int argc,char *argv[])
 {   if(argc<2)
     std::cout<<"Usage is ./header.exe {filename} "<<std::endl;
@@ -74,6 +97,14 @@ int main(int argc,char *argv[])
         std::string LocalizationId;
         std::string HeaderSize;
         std::string FolderName;
+        std::string PackageFlags;
+        // std::string NameCount;
+        // std::string NameOffset;
+
+
+        int NameCount;
+        int NameOffset;
+
    
         header = finder(16,file);
         std::cout<<"Header: "<<header<<std::endl;
@@ -97,6 +128,9 @@ int main(int argc,char *argv[])
         unsigned long long versionKeylength = versions*16;
         unsigned long long total_version_bytes_length = versions*4;
         int totalCustomVersionLength = versionKeylength + total_version_bytes_length;
+        std::cout<<"1: "<<totalCustomVersionLength<<std::endl;
+        std::cout<<"2: "<<versionKeylength<<std::endl;
+        std::cout<<"3: "<<total_version_bytes_length<<std::endl;
         fseek(file,ftell(file) + totalCustomVersionLength,SEEK_SET); 
         std::cout<<ftell(file)<<std::endl;
 
@@ -112,12 +146,21 @@ int main(int argc,char *argv[])
         std::cout<<hexToAscii(FolderName)<<std::endl;
         std::cout<<ftell(file)<<std::endl;
 
-        std::cout<<fseek(file,ftell(file)+ 16,SEEK_SET)<<std::endl;
+        PackageFlags = info_finder(4,file);
+        std::cout<<"PackageFlags: "<<stoul((PackageFlags),0,16)<<std::endl;
+        NameCount = stoi(info_finder(4,file));
+        std::cout<<"NameCount: "<<NameCount<<std::endl; 
+        NameOffset = stoi(info_finder(4,file),0,16);
+        std::cout<<"NameOffset: "<<NameOffset<<std::endl; 
+        long int pos= fseek(file,ftell(file)+4,SEEK_SET);
+        //Localization ID
+        //first name from 502-540
+        std::cout<<"Names: "<<std::endl;
+        printNames(NameOffset,10,file); //NameCount will be the required argument
+
         
 
-        LocalizationId = finder(32,file);
-        std::cout<<hexToAscii(LocalizationId)<<std::endl;
-        std::cout<<ftell(file)<<std::endl;
+
 
 
         
